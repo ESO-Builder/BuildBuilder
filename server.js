b83292515d221daf5061a785ec4068789e5c4f71
enzotiezzi <enzo_tiezzi@hotmail.com>
2016-07-02 01:15:21 -0300

    ajustando rotas

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var SampleApp = function() {
             //  allows us to run/test the app locally.
             console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
             self.ipaddress = "127.0.0.1";
-        };
+        }
     };
 
 
